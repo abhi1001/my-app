eb05c38932021ade4cdf5f82947915fb53dafe3c
Abhishek Agrawal <abhi9.don@gmail.com>
2018-08-31 05:05:04 +0530

    3

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+router.get('/client', function(req, res, next) {
+  res.render('client');
+});
+router.get('/map', function(req, res, next) {
+  res.render('map');
+});
+router.get('/scandetails', function(req, res, next) {
+  res.render('scandetails');
+});
 module.exports = router;
